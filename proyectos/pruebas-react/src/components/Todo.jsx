011d830975f829339a306d9ff5cde059aeb4fa3d
linareswst <oscar.alejandro.linares@gmail.com>
2023-08-20 20:28:40 -0600

    Finalizando proyecto

---
+++ proyectos/pruebas-react/src/components/Todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function Todo({todo, index, deleteTodo}){
+    return (
+        <>
+            <div className='list'>
+                <h3>{todo}</h3> <button className='btn-delete' onClick={() => deleteTodo(index)}>x</button>
+            </div>
+        </>
+    )
+}
+
+export default Todo
